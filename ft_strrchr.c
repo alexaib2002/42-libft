b38e8a929e59ede89fdc613f64992fd042d895a8
alexaib <alexaib@outlook.es>
2023-01-15 11:11:45 +0100

    Fixed typo

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,11 +6,11 @@
 /*   By: alexaib <alexaib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 20:05:53 by alexaib           #+#    #+#             */
-/*   Updated: 2023/01/14 20:10:41 by alexaib          ###   ########.fr       */
+/*   Updated: 2023/01/15 10:46:49 by alexaib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h";
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
