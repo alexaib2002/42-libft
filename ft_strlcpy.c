fc446e2327f461aa1a6ce386bd75fac757c6b0f9
alexaib <alexaib@outlook.es>
2023-01-24 17:54:09 +0100

    Refactored strlcpy by using memcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 09:46:02 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/20 12:06:57 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/01/24 17:53:52 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,11 @@
 
 int	ft_strlcpy(char *dst, const char *src, int size)
 {
-	int	i;
-	int	cp_len;
+	const int	slen = ft_strlen(src);
 
-	i = 0;
-	cp_len = ft_strlen(src);
 	if (size == 0)
-		return (cp_len);
-	while (src[i] && i < size - 1)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = 0;
-	return (cp_len);
+		return (slen);
+	ft_memcpy(dst, src, size);
+	*(dst + ft_strlen(dst)) = 0;
+	return (slen);
 }
