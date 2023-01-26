029fa9737d25a4628cd5e67fe858f4c2bdbec73a
alexaib <alexaib@outlook.es>
2023-01-26 12:35:18 +0100

    Implemented lstlast

---
+++ ft_lstlast_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 12:30:15 by aaibar-h          #+#    #+#             */
+/*   Updated: 2023/01/26 12:34:46 by aaibar-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_bonus.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst->next)
+		return (ft_lstlast(lst->next));
+	else
+		return (lst);
+}
