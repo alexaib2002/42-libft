803af21ccf4fe15ba9f180b25b187dc7dd53cca4
alexaib <alexaib@outlook.es>
2023-01-28 18:26:11 +0100

    [Bugfix] strmapi did not NUL terminate the string

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 11:20:05 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/24 18:36:41 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/01/28 18:25:24 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 		str[i] = f(i, s[i]);
 		i++;
 	}
+	str[i] = 0;
 	return (str);
 }
