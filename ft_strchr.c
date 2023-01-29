ffc56680ffca8b86122492c1fc44dbb04a38be4a
alexaib <alexaib@outlook.es>
2023-01-29 17:01:57 +0100

    Refactored strchr to use recursive calls Added cast 'c' to char

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 21:27:14 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/24 18:50:50 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/01/29 17:00:31 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
-	{
-		if (*s == c)
-			return ((char *) s);
-		s++;
-	}
-	return (0);
+	if (*s == (char) c)
+		return ((char *) s);
+	if (!*s)
+		return (0);
+	return (ft_strchr(s + 1, c));
 }
