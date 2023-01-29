9aa7bd1f917f82da7e9fb183361d2def790cfb49
alexaib <alexaib@outlook.es>
2023-01-29 12:19:44 +0100

    [Refactor] Fixed overflows with MIN_INT on itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 13:51:35 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/21 12:58:22 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/01/29 12:14:59 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ static int	ft_numdigs(int n)
 	int	d;
 
 	d = 1;
-	if (n < 0)
-		n *= -1;
-	while (n > 9)
+	while (n / 10)
 	{
 		n /= 10;
 		d++;
@@ -28,42 +26,36 @@ static int	ft_numdigs(int n)
 	return (d);
 }
 
-static char	*ft_istralloc(int n)
+static char	*ft_stralloc(int n)
 {
-	int		digs;
-	int		exdigs;
+	int		strsize;
 
-	exdigs = 1;
-	digs = ft_numdigs(n);
+	strsize = ft_numdigs(n) + 1;
 	if (n < 0)
-	{
-		n *= -1;
-		exdigs++;
-	}
-	return (malloc((digs + exdigs) * sizeof(char)));
+		strsize++;
+	return (malloc((strsize) * sizeof(char)));
 }
 
 char	*ft_itoa(int n)
 {
-	char	*str;
-	int		i;
-	int		d;
+	char			*str;
+	int				i;
+	int				z;
 
-	str = ft_istralloc(n);
+	str = ft_stralloc(n);
 	i = 0;
-	d = 1;
-	while (ft_numdigs(d) < ft_numdigs(n))
-		d *= 10;
+	z = 1;
+	while (ft_numdigs(z) < ft_numdigs(n))
+		z *= 10;
 	if (n < 0)
 	{
 		str[i++] = '-';
-		n *= -1;
 	}
-	while (d)
+	while (z)
 	{
-		str[i++] = (n / d) + NUMS_PAD;
-		n -= d * (n / d);
-		d /= 10;
+		str[i++] = ft_abs(n / z) + NUMS_PAD;
+		n -= z * (n / z);
+		z /= 10;
 	}
 	str[i] = 0;
 	return (str);
