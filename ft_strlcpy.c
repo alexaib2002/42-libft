a1e8bf401ef973deafa353199bae70f78c4d74f7
alexaib <alexaib@outlook.es>
2023-01-29 13:18:28 +0100

    [Hotfix] Changed arguments on clamp

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 09:46:02 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/29 13:00:23 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/01/29 13:11:39 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	const size_t	len = ft_clamp(0, size, ft_strlen(src) + 1);
+	const size_t	len = ft_clamp(0, ft_strlen(src), size - 1);
 
 	if (!size)
 		return (ft_strlen(src));
 	ft_memcpy(dst, src, len);
-	*(dst + len - 1) = 0;
+	*(dst + len) = 0;
 	return (ft_strlen(src));
 }
