65646664fafa9c9314bc36b54435738b8f92a9cf
alexaib <alexaib@outlook.es>
2023-02-01 15:10:05 +0100

    Reimplemented putnbr without itoa

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 18:12:37 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/29 11:42:29 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/02/01 14:56:51 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
+	int	z;
 
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
-	free(str);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+	}
+	z = 1;
+	while (ft_ndigs(z) < ft_ndigs(n))
+		z *= 10;
+	while (z)
+	{
+		ft_putchar_fd(ft_abs(n / z) + NUMS_PAD, fd);
+		n -= z * (n / z);
+		z /= 10;
+	}
 }
