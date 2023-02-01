a2aa2041a7215a08da35a9df067e04373ff288cd
alexaib <alexaib@outlook.es>
2023-02-01 22:22:57 +0100

    Split refactor finished, most tests passed TODO substr allocation is not protected during split operation

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 12:25:38 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/31 23:44:03 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/02/01 22:26:08 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,48 +29,47 @@ static size_t	ft_splitn(const char *trstr, char c)
 	n = 1;
 	i = 0;
 	while (trstr[i])
-		if (trstr[i++] == c)
+	{
+		if (trstr[i] == c && trstr[ft_clamp(0, ft_strlen(trstr), i + 1)] != c)
 			n++;
+		i++;
+	}
 	return (n);
 }
 
-/*
-TODO
-*** REMOVE ME LATER ***
-TODO
-Implement new function is_strempty so split may detect when new split is
-required in case range is empty
-
-Check that every split will contain a complete string before calling malloc
-
-strchr(s, 0) == s   // checks if the NUL terminator equals the current pointer
-AKA the pointer is empty!!
-*/
-
-char	**ft_split(const char *s, char c)
+char	**ft_gensplit(const char *str, char c)
 {
 	char	**strarr;
 	char	*next;
-	char	*c_str;
-	size_t	size;
 	size_t	i;
+	size_t	size;
 
-	s = ft_strtrim(s, &c);
-	c_str = (char *) s;
-	size = ft_splitn(s, c);
+	size = ft_splitn(str, c);
 	strarr = malloc((size + 1) * sizeof(char *));
 	if (!strarr)
 		return (NULL);
 	i = 0;
-	while (i < size)
+	while (i < size && *str)
 	{
-		next = ft_strchr(s, c);
+		next = ft_strchr(str, c);
 		if (!next)
-			next = ft_strlen(s) + (char *) s;
-		strarr[i++] = ft_substr(s, 0, next - s);
-		s = next + 1;
+			next = ft_strlen(str) + (char *) str;
+		strarr[i++] = ft_substr(str, 0, next - str);
+		str = next;
+		while (*str == c)
+			str++;
 	}
-	free(c_str);
 	strarr[i] = NULL;
+	return (strarr);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**strarr;
+	char	*trstr;
+
+	trstr = ft_strtrim(s, &c);
+	strarr = ft_gensplit(trstr, c);
+	free(trstr);
 	return (strarr);
 }
