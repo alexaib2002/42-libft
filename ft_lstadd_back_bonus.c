213f49d2395fbe9b6047005a347877b401177ad2
alexaib <alexaib@outlook.es>
2023-02-04 18:51:01 +0100

    Added NULL checkers on lstaddback

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 12:22:23 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/27 09:44:06 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/02/04 18:00:38 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	if (!lst || !*lst || !new)
+		return ;
 	ft_lstlast(*lst)->next = new;
 }
