7170b2526c555e43e0f134918fcb920a1728bdd6
alexaib <alexaib@outlook.es>
2023-02-04 19:47:11 +0100

    Reset pointer to NULL on lstclear

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 13:21:13 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/27 13:35:27 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/02/04 19:25:32 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,5 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	if ((*lst)->next)
 		ft_lstclear(&((*lst)->next), del);
 	ft_lstdelone(*lst, del);
+	*lst = NULL;
 }
