73f4377ab44397b931f0b50b7e6a1f8f2be97416
alexaib <alexaib@outlook.es>
2023-02-05 18:10:24 +0100

    Added NULL protection on lstlast

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 12:30:15 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/26 12:34:46 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/02/05 18:09:43 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
+	if (!lst)
+		return (NULL);
 	if (lst->next)
 		return (ft_lstlast(lst->next));
 	else
