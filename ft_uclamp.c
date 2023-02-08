d66aecdc4a058a5f50bfbba65375b7462fe438cc
alexaib <alexaib@outlook.es>
2023-02-08 22:51:44 +0100

    Refactored strlcpy with new uclamp function

--- ft_uclamp.c
+++ ft_uclamp.c
@@ -1,24 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_uclamp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/13 09:46:02 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/02/08 22:50:29 by aaibar-h         ###   ########.fr       */
+/*   Created: 2023/02/08 22:47:47 by aaibar-h          #+#    #+#             */
+/*   Updated: 2023/02/08 22:48:16 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_uclamp(size_t min, size_t max, size_t n)
 {
-	const size_t	len = ft_uclamp(0, ft_strlen(src), size - 1);
-
-	if (!size)
-		return (ft_strlen(src));
-	ft_memcpy(dst, src, len);
-	*(dst + len) = 0;
-	return (ft_strlen(src));
+	if (n < min)
+		n = min;
+	if (n > max)
+		n = max;
+	return (n);
 }
