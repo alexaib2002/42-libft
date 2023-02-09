bb53842f1298508c4a3c27c6aab47f990564499e
alexaib <alexaib@outlook.es>
2023-02-09 23:34:15 +0100

    Some refactors, needs more testing

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: alexaib <alexaib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 11:32:58 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/01/29 20:00:20 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/02/09 23:29:22 by alexaib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	char		*ptr;
-	size_t		len;
-	const int	dlen = ft_strlen(dst);
+	char			*ptr;
+	size_t			len;
+	const size_t	dlen = ft_strlen(dst);
 
-	len = ft_clamp(0, ft_strlen(src), size - ft_strlen(dst) - 1);
-	ptr = dst + ft_strlen(dst);
+	len = ft_clamp(0, ft_strlen(src), size - dlen - 1);
+	ptr = dst + dlen;
 	ft_memcpy(ptr, src, len);
 	*(ptr + len) = 0;
+	if (size > dlen)
+		size = dlen;
 	return (ft_clamp(0,
 			dlen + ft_strlen(src), size + ft_strlen(src)));
 }
